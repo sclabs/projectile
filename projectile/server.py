sclabs/projectile

commit 1fc5c0a5d868763de36d47d98891f5d2da2a5aa0

fixes #3
uses tox to test py27 and py36 and lint

this commit tests whether or not running tox in github actions will
actually trigger the lint test

this commit contains an intentional style error which should fail the
github action if it actually runs the linter

projectile/server.py
@@ -1,6 +1,6 @@
 import argparse
-from six.moves import cStringIO as StringIO
 import os
+from io import BytesIO
 
 from tornado import ioloop, web
 from PIL import Image
@@ -161,10 +161,9 @@ class TileHandler(web.RequestHandler):
         image : PIL.Image
             The Image to respond with.
         """
-        stream = StringIO()
+        stream = BytesIO()
         image.save(stream, format='png')
-        for line in stream.getvalue():
-            self.write(line)
+        self.write(stream.getvalue())
         self.set_header('Content-type', 'image/png')
 
 
@@ -282,7 +281,7 @@ def main():
         color the tiles.''')
     parser.add_argument(
         '-t', '--tile_size', type=int, default=256, help='''The resolution of
-        image tiles to serve, in pixels. Must be a power of 2. The default is 
+        image tiles to serve, in pixels. Must be a power of 2. The default is
         256 for 256 x 256 pixel image tiles.''')
     parser.add_argument(
         '--client', help='''Specify a custom client HTML file to serve.''')
@@ -315,7 +314,6 @@ def main():
     print('starting server at http://localhost:%s/' % args.port)
     run(array, cmap=args.cmap, tile_size=args.tile_size, client=args.client,
         port=args.port, debug=args.debug)
-
 
 if __name__ == '__main__':
     main()

.github/workflows/tests.yaml
@@ -1,0 +1,22 @@
+name: Tests
+
+on: [push]
+
+jobs:
+  build:
+
+    runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        python: [2.7, 3.6]
+
+    steps:
+      - uses: actions/checkout@v2
+      - name: Setup Python
+        uses: actions/setup-python@v1
+        with:
+          python-version: ${{ matrix.python }}
+      - name: Install Tox and any other packages
+        run: pip install tox
+      - name: Run Tox
+        run: tox -e py  # Run tox using the version of Python in `PATH`

.gitignore
@@ -4,3 +4,5 @@ venv
 *.egg-info
 dist
 build
+.tox
+images

projectile/get_test_data.py
@@ -1,4 +1,4 @@
-import urllib
+from six.moves.urllib.request import urlretrieve
 import os
 
 from projectile.image_to_array import run as image_to_array
@@ -7,10 +7,10 @@ from projectile.image_to_array import run as image_to_array
 def main():
     if not os.path.exists('images'):
         os.makedirs('images')
-    urllib.urlretrieve(
+    urlretrieve(
         'http://sipi.usc.edu/database/download.php?vol=aerials&img=2.2.17',
         'images/sanfran.tiff')
-    urllib.urlretrieve(
+    urlretrieve(
         'http://sipi.usc.edu/database/download.php?vol=aerials&img=3.2.25',
         'images/pentagon.tiff')
     image_to_array('images/sanfran.tiff', mode='RGB')

setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
-from codecs import open
+from io import open
 from os import path
 
 import versioneer

tests/test_server.py
@@ -1,0 +1,18 @@
+import numpy as np
+from tornado.testing import AsyncHTTPTestCase
+
+from projectile.server import make_app, get_test_data
+
+
+class TestServer(AsyncHTTPTestCase):
+    def get_app(self):
+        get_test_data()
+        return make_app(np.load('images/sanfran.npy'))
+
+    def test_client(self):
+        response = self.fetch('/')
+        self.assertEqual(response.code, 200)
+
+    def test_tile(self):
+        response = self.fetch('/3/2/3/256.png')
+        self.assertEqual(response.code, 200)

tox.ini
@@ -1,0 +1,10 @@
+[tox]
+envlist = py27,py36,lint
+
+[testenv]
+deps = pytest
+commands = pytest
+
+[testenv:lint]
+deps = flake8
+commands = flake8 projectile

commit 7edc7fd44310fafa79a30660b33eb30ef99c7bdd

fixing intentional style error introduced in 1fc5c0a

projectile/server.py
@@ -315,5 +315,6 @@ def main():
     run(array, cmap=args.cmap, tile_size=args.tile_size, client=args.client,
         port=args.port, debug=args.debug)
 
+
 if __name__ == '__main__':
     main()